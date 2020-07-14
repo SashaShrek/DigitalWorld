87b2ede9c8992bcafb65fefbd772804e09116ca7
SashaShrek <jmatyushin@gmail.com>
2020-07-14 18:04:15 +0300

    Changed kernel of the site

--- scripts.js
+++ scripts.js
@@ -1,27 +1,33 @@
-function load(){
-    let size = document.getElementById('f0').clientWidth;
-    let proc = document.getElementById('f0').clientWidth * 0.01;
-    aFacile(size, proc, 0);
-    size = document.getElementById('p0').clientWidth;
-    proc = document.getElementById('p0').clientWidth * 0.01;
-    aPrec(size, proc, 0);
-}
-function aFacile(s, p, i){
-    if(i == Math.ceil(s * 4 + p * 3)){
-        document.getElementById('facileImg').scrollLeft = -i;
-        
-        setTimeout(aFacile, 50, s, p, 0);
-    }else{
-        document.getElementById('facileImg').scrollLeft = i;
-        setTimeout(aFacile, 13, s, p, i + 1);
+class Loader{
+    Load(){
+        let animation = new Animation();
+        let size = $("#f0").clientWidth;
+        let proc = $('#f0').clientWidth * 0.01;
+        animation.aFacile(size, proc, 0);
+        size = $('#p0').clientWidth;
+        proc = $('#p0').clientWidth * 0.01;
+        animation.aPrec(size, proc, 0);
     }
 }
-function aPrec(s, p, i){
-    if(i == Math.ceil(s * 4 + p * 3)){
-        document.getElementById('precessionImg').scrollLeft = -i;
-        setTimeout(aPrec, 15, s, p, 0);
-    }else{
-        document.getElementById('precessionImg').scrollLeft = i;
-        setTimeout(aPrec, 15, s, p, i + 1);
+
+class Amimation{
+    aFacile(s, p, i){
+        if(i == Math.ceil(s * 4 + p * 3)){
+            $('#facileImg').scrollLeft = -i;
+            
+            setTimeout(aFacile, 50, s, p, 0);
+        }else{
+            $('#facileImg').scrollLeft = i;
+            setTimeout(aFacile, 13, s, p, i + 1);
+        }
+    }
+    aPrec(s, p, i){
+        if(i == Math.ceil(s * 4 + p * 3)){
+            $('#precessionImg').scrollLeft = -i;
+            setTimeout(aPrec, 15, s, p, 0);
+        }else{
+            $('#precessionImg').scrollLeft = i;
+            setTimeout(aPrec, 15, s, p, i + 1);
+        }
     }
 }
